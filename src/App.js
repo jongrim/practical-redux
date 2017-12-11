f2f07a98e2079fd1b9df8fc3c8ea8f2fda4d4875
Jonathan Grim <jonjongrim@gmail.com>
2017-12-11 07:39:52 -0500

    Remove css

--- src/App.js
+++ src/App.js
@@ -1,18 +1,16 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import { Header } from 'semantic-ui-react';
 import './App.css';
-
-import SampleComponent from './SampleComponent';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Project Mini-Mek</h2>
-        </header>
-        <SampleComponent />
+        <div className="App-header">
+          <Header inverted as="h1">
+            Project Mini-Mek
+          </Header>
+        </div>
       </div>
     );
   }
